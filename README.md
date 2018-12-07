f30d9c26a70de6f51f97c1b0e0055a5e5e41d085
yingfu.chen <yingfu.chen@zcplan.cn>
2018-12-07 16:34:40 +0800

    add php-fpm options

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# docker-php-fpm
-php-fpm docker 文件
+
+###  docker image for php contain nginx  and supervisor
+
+common php program can use this image:  
+
+yinfu/php-nginx:latest
+
+```
+
+docker run -d -p 80:80  -v /www:/var/www/html yinfu/php-nginx:latest
+
+```
+            
+
+
+if you use thinkphp5 program ,you can use this image :  
+yinfu/php-nginx:thinkphp5
+
+```
+docker run -d -p 80:80  -v /www:/var/www/html yinfu/php-nginx:thinkphp5
+```
+
+
+if you want to diy php-fpm options  ,you can use ENV  to set some options 
+
+
+```
+
+
+docker run -d -p 80:80  -v /www:/var/www/html -e PM=dynamic -e MAX_CHILDREN=30 -e START_SERVERS=15 -e MIN_SPARE_SERVERS=11 -e MAX_SPARE_SERVERS=30 -e MAX_REQUESTS=1024 yinfu/php-nginx:thinkphp5
+
+```
--- start.sh
+++ start.sh
@@ -1,4 +1,38 @@
 #!/bin/bash
+
+#php-fpm 参数调优
+
+if [ -z "$PM" ]; then
+  PM="dynamic"
+fi
+awk -v LINE="pm = $PM" '{ sub(/^pm = dynamic*/, LINE); print; }' /usr/local/etc/php-fpm.d/www.conf > /usr/local/etc/php-fpm.d/www.conf.tmp && mv /usr/local/etc/php-fpm.d/www.conf.tmp /usr/local/etc/php-fpm.d/www.conf
+
+if [ -z "$MAX_CHILDREN" ]; then
+  MAX_CHILDREN=30
+fi
+awk -v LINE="pm.max_children=$MAX_CHILDREN" '{ sub(/^pm.max_children = 5*/, LINE); print; }' /usr/local/etc/php-fpm.d/www.conf > /usr/local/etc/php-fpm.d/www.conf.tmp && mv /usr/local/etc/php-fpm.d/www.conf.tmp /usr/local/etc/php-fpm.d/www.conf
+
+if [ -z "$START_SERVERS" ]; then
+  START_SERVERS=15
+fi
+awk -v LINE="pm.start_servers=$START_SERVERS" '{ sub(/^pm.start_servers = 2*/, LINE); print; }' /usr/local/etc/php-fpm.d/www.conf > /usr/local/etc/php-fpm.d/www.conf.tmp && mv /usr/local/etc/php-fpm.d/www.conf.tmp /usr/local/etc/php-fpm.d/www.conf
+
+if [ -z "$MIN_SPARE_SERVERS" ]; then
+  MIN_SPARE_SERVERS=11
+fi
+awk -v LINE="pm.min_spare_servers=$MIN_SPARE_SERVERS" '{ sub(/^pm.min_spare_servers = 1*/, LINE); print; }' /usr/local/etc/php-fpm.d/www.conf > /usr/local/etc/php-fpm.d/www.conf.tmp && mv /usr/local/etc/php-fpm.d/www.conf.tmp /usr/local/etc/php-fpm.d/www.conf
+
+if [ -z "$MAX_SPARE_SERVERS" ]; then
+  MAX_SPARE_SERVERS=30
+fi
+awk -v LINE="pm.max_spare_servers=$MAX_SPARE_SERVERS" '{ sub(/^pm.max_spare_servers = 3*/, LINE); print; }' /usr/local/etc/php-fpm.d/www.conf > /usr/local/etc/php-fpm.d/www.conf.tmp && mv /usr/local/etc/php-fpm.d/www.conf.tmp /usr/local/etc/php-fpm.d/www.conf
+
+if [ -z "$MAX_REQUESTS" ]; then
+  MAX_REQUESTS=1024
+fi
+awk -v LINE="pm.max_requests=$MAX_REQUESTS" '{ sub(/^;pm.max_requests = 500*/, LINE); print; }' /usr/local/etc/php-fpm.d/www.conf > /usr/local/etc/php-fpm.d/www.conf.tmp && mv /usr/local/etc/php-fpm.d/www.conf.tmp /usr/local/etc/php-fpm.d/www.conf
+
+
 #/etc/init.d/cron start
 /etc/init.d/supervisor start
 /usr/local/nginx/sbin/nginx 
